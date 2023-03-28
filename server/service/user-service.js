aca4ac97f7dbbf316806cb5446a2673e8fb0232a
EugeneVakarchuk <eugenevakarchuk@gmail.com>
2023-03-29 00:24:55 +0300

    created logout functionality

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -46,6 +46,11 @@ class UserService {
       user: userDto
     }
   }
+
+  async logout(refreshToken) {
+    const token = await tokenService.removeToken(refreshToken);
+    return token;
+  }
 }
 
 
